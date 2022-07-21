4d6cc617cb74c2e2b555a7363dbe4ea39fc1d71b
tarper24 <tarper24@tarper24.net>
2022-07-21 00:17:29 -0500

    ✨ Add shortlink and subdomain redirecting

--- src/index.js
+++ src/index.js
@@ -9,7 +9,30 @@
  */
 
 export default {
-	async fetch(request) {
-		return new Response("Hello World!");
+	async fetch(request, env) {
+		const connectingIP = request.headers.get('CF-Connecting-IP');
+		const referer = request.headers.get('referer');
+		const userAgent = request.headers.get('User-Agent');
+
+		const requestURL = new URL(request.url);
+		const subdomain = requestURL.hostname.replace(RegExp(`\.?${env.BASE_HOSTNAME}$`), "");
+		const pathname = requestURL.pathname.replace(/^\//, "");
+
+		let redirectURL = await env.SHORTLINKS.get("default");
+		const uuid = crypto.randomUUID();
+
+		if (["go", "cf-shortlinks"].includes(subdomain)) {
+			if (pathname !== "") {
+				redirectURL = await env.SHORTLINKS.get(pathname) || redirectURL;
+			}
+		}
+		else if (subdomain !== "") {
+			redirectURL = await env.SHORTDOMAINS.get(subdomain) || redirectURL;
+		}
+
+		console.log(`${uuid} | ${connectingIP} - [${new Date().toISOString()}] "${request.method} ${request.url} ${request.cf.httpProtocol}" ${env.REDIRECT_CODE} "${referer || "-"}" "${userAgent || "-"}" `);
+		console.log(`${uuid} | ${request.cf.city}, ${request.cf.regionCode || request.cf.region}, ${request.cf.country} - ${redirectURL}`);
+
+		return Response.redirect(redirectURL, env.REDIRECT_CODE);
 	},
 };
